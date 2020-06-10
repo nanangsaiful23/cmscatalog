656d8e53e2439f7e0a28f06fdd2c6bb863976aac
nanangsaiful23 <nanangsaiful23@gmail.com>
2020-06-10 07:03:40 +0700

    delete routes in web

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['middleware' => 'cors'], function() {
-    Route::get('all', 'ProductController@getAllProduct');
-});
 
 Auth::routes();
 
